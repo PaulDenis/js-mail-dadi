512ae8e41cb970128b5a84f67630ba56e0af4106
PaulDenis <d.paul@outlook.it>
2021-04-28 16:05:57 +0200

    Progetto iniziato.

---
+++ script/script.js
@@ -0,0 +1,6 @@
+// 1 - Mail
+// Chiedi all'utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull'esito del controllo.
+
+var allowed_email = ["d.paul@outlook.it", "paw_lion@live.it", "classe_34@gmail.com", "evangelista.vince@gmail.it"];
